01c89b2e6e8d884ee7c202afc14427c6b33ee846
Joseph Bangoura <ptijjo@hotmail.com>
2025-01-23 21:42:08 +0100

    ajoute du fichier Analytics

---
+++ app/Analytics.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useEffect } from "react";
+import Script from "next/script";
+import { usePathname } from "next/navigation";
+
+const GA_MEASUREMENT_ID = "G-6PLXY6GSBW"; // Remplacez par votre ID de mesure
+
+export default function Analytics() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.gtag) {
+      window.gtag("config", GA_MEASUREMENT_ID, {
+        page_path: pathname,
+      });
+    }
+  }, [pathname]);
+
+  return (
+    <>
+      {/* Ajout des scripts Google Analytics */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
-import Script from "next/script";
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
+import Analytics from "./Analytics";
+
 
 
 
@@ -20,37 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
-  const pathname = usePathname();
-
-  useEffect(() => {
-    if (typeof window !== "undefined" && window.gtag) {
-      window.gtag("config", GA_MEASUREMENT_ID, {
-        page_path: pathname,
-      });
-    }
-  }, [pathname]);
 
   return (
     <StoreProvider>
       <html lang="fr">
-        <head>
-          {/* Ajout des scripts Google Analytics */}
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_MEASUREMENT_ID}', {
-                page_path: window.location.pathname,
-              });
-            `}
-          </Script>
-        </head>
         <body className="flex flex-col max-w-7xl items-center m-auto">
+        <Analytics />
           {children}
         </body>
       </html>
